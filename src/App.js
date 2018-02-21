2332ab9e932eb40fe0171b025294a0259f5d40c3
ctrachte <cetrachte@hotmail.com>
2018-02-21 15:02:20 -0600

    add constructor to component, add initial state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 // Now that it's been imported, we can use it as a tag in the JSX that's returned by the  App component's render() method.
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
