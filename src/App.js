9cfcdc1bbbb3e00a89cf9842660077e9cd36b50f
ctrachte <cetrachte@hotmail.com>
2018-02-20 20:19:46 -0600

    import ToDo list to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
+// import the ToDo component.
+import ToDo from './components/ToDo.js';
+// Now that it's been imported, we can use it as a tag in the JSX that's returned by the  App component's render() method.
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
       </div>
     );
   }
