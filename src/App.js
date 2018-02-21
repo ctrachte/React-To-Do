9c62aeb1de83ae84e67934bc7d6e721a09840c61
ctrachte <cetrachte@hotmail.com>
2018-02-21 15:12:57 -0600

    add map fn to render to-do objects

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import ToDo from './components/ToDo.js';
 class App extends Component {
   constructor(props) {
     super(props);
+    // Now that there's a constructor function, we can assign the component's initial state using the this keyword.
+    // A component's state is expected to be an object, so assign an object with a todos property to this.state. Make todos an array, and add several objects to the array.
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Walk the cat', isCompleted: true }, //Each object will be a to-do with properties named  description and isCompleted.
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
       ]
@@ -20,6 +22,11 @@ class App extends Component {
         <ul>
           <ToDo />
           <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } />
+            // The .map() method iterates through the items in an array and returns a new array,
+            // and we can use it to convert raw data into an array of JSX elements that React will render into HTML.
+          )}
         </ul>
       </div>
     );
